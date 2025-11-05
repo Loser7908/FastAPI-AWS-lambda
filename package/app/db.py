daad92bff9aa48336af7823dc072d3830fc3edc4
Loser7908 <90123676+Loser7908@users.noreply.github.com>
2025-11-05 11:22:42 +0530

    Delete package directory

--- package/app/db.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import os
-
-dynamodb = boto3.resource(
-    "dynamodb",
-    region_name="us-east-1"
-)
-
-table = dynamodb.Table("NotesTable")
--- package/app/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class NoteIn(BaseModel):
-    title: str
-    content: str
-
-class Note(NoteIn):
-    id: str
